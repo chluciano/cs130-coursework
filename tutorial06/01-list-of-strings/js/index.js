cb2adecec0802d11c6f9e7794e6b98f656007097
chluciano <christian.luciano7@gmail.com>
2020-05-14 16:08:41 -0500

    tutorial 6

---
+++ tutorial06/01-list-of-strings/js/index.js
@@ -0,0 +1,9 @@
+const names = [
+    "Jane", "Brenda", "Wanda", "Maria", "Jasper",
+    "John", "Malik", "Arjun", "Larry", "Curly", "Moe"
+];
+
+
+names.forEach(name => {
+	document.write(`<p>Welcome, ${name}! </p>`)
+});
